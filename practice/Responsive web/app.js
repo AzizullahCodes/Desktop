bebc123162f9db09ae4df57fe1248d1043bf8ccc
AzizullahCodes <azizullahaziz7777@gmail.com>
2025-04-17 15:45:08 +0500

    y

--- practice/Responsive web/app.js
+++
@@ -1,5 +0,0 @@
-var tablinks = document.getElementsByClassName("tab-links");
-var tabcontents =document.getElementsByClassName("tab-contents");
-function opentab(tabname){
-    for(tablink of tabliks){tablink.classList.remove("active-link");}
-}
